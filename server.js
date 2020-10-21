7d1c512db3e61589c318e7789979c3031a70b839
olcay <olcayusta02@gmail.com>
2020-10-21 19:51:06 +0300

    commited

--- server.js
+++ server.js
@@ -17,22 +17,25 @@ wss.on('connection', (ws) => {
   ws.on('message', async (data) => {
     const extractData = JSON.parse(data)
     const { payload } = extractData
-    const sql = `WITH CTE AS
-                              (
-                                  INSERT INTO chat_message ("roomId", "userId", content)
-                                      VALUES ($1, $2, $3)
-                                      RETURNING id, "roomId", "userId", content, "creationTime", type
-                              )
-                     SELECT CTE.id,
-                            CTE."roomId",
-                            CTE.content,
-                            CTE."creationTime",
-                            CTE.type,
-                            (SELECT row_to_json(u) FROM "user" u WHERE u.id = CTE."userId") AS "user"
-                     FROM CTE
-        `
-    const values = [1, 1, payload.content]
-    const { rows } = await pool.query(sql, values)
+    const queryText = {
+      text: `
+                WITH CTE AS
+                         (
+                             INSERT INTO chat_message ("roomId", "userId", content)
+                                 VALUES ($1, $2, $3)
+                                 RETURNING id, "roomId", "userId", content, "creationTime", type
+                         )
+                SELECT CTE.id,
+                       CTE."roomId",
+                       CTE.content,
+                       CTE."creationTime",
+                       CTE.type,
+                       (SELECT row_to_json(u) FROM "user" u WHERE u.id = CTE."userId") AS "user"
+                FROM CTE
+            `,
+      values: [1, 1, payload.content],
+    }
+    const { rows } = await pool.query(queryText)
 
     const formatJSON = {
       event: 'message',
